8af7cbd2b33e7348abc132a786eee58d7ec938f1
Aloushek <aloushek@outlook.com>
2023-02-12 15:59:54 +0100

    Ukol part 2 - Header

---
+++ src/components/header/header.jsx
@@ -0,0 +1,11 @@
+import camera from "./imgs/camera.svg";
+import React from "react";
+
+export const Header = () => (
+    <>
+        <h1>
+            <img class="logo" src={camera} alt="Logo" />
+            V našem kině právě uvádíme
+        </h1>
+    </>
+);
